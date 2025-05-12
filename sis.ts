e810f7fe8b432c449bbec0612fc6e4eaaff8d67d
yahooserg <seltexinc@gmail.com>
2025-05-12 14:37:09 +0700

    delete type

--- sis.ts
+++ sis.ts
@@ -356,7 +356,6 @@ app.post('/api/getrecommendedurlforitem/company/:company/description/:descriptio
 });
 
 app.get('/api/createxlprice', function(req: any, res) {
-  req.params.company = "";
   mySqlService.getPriceListData(req.params.company, (priceListData: any) => {
     myXLService.createXLPrice(priceListData, (xlFile: any)=>{
         myAWSService.uploadPrice(xlFile, ()=>{
