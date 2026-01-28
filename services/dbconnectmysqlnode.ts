e671909b834997c3507283719fa2e46517f7a4db
yahooserg <seltexinc@gmail.com>
2026-01-28 15:46:17 +0700

    stupid work

---
+++ services/dbconnectmysqlnode.ts
@@ -0,0 +1,6 @@
+export default class MySqlConnection {
+  host: string = 'seltex.ru';
+  user: string = 'Guest';
+  password: string = 'Q12345z';
+  database: string = 'seltexru';
+}
--- sis.ts
+++ sis.ts
@@ -7,7 +7,7 @@ const app = express();
 import bodyParser from 'body-parser';
 // const fs = require('fs');
 import * as fs from 'fs';
-import { MyNodeConfig } from './config/mynodeconfig.ts';
+import MyNodeConfig from './config/mynodeconfig.ts';
 const myNodeConfig = new MyNodeConfig();
 import { MySqlService } from './services/mysql.service.ts';
 const mySqlService = new MySqlService();
@@ -40,6 +40,7 @@ if (!myNodeConfig.secure) {
 }
 //////////////////////////////////////////////
 
+// app.use(bodyParser.urlencoded({ extended: false },{limit: '5mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({limit: '5mb'}));
 
